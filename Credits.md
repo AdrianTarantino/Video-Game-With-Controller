273ac11f4e8a4bc697ca3bf942c5d340a0fc7086
AdrianTheHacker <57376726+AdrianTheHacker@users.noreply.github.com>
2024-06-05 21:01:35 -0400

    Adding Credits and Adjusting Values

---
+++ Credits.md
@@ -0,0 +1,3 @@
+# Credits
+
+- Serial Communication Code: [This Arduino Project](https://projecthub.arduino.cc/ansh2919/serial-communication-between-python-and-arduino-663756)
--- Video-Game-Codebase/src/main.py
+++ Video-Game-Codebase/src/main.py
@@ -5,11 +5,11 @@ arduino: serial.Serial = serial.Serial(port='COM4', baudrate=115200, timeout=0.1
 
 def writeReadTest(writeData: str) -> any:
   arduino.write(bytes(writeData, 'utf-8'))
-  time.sleep(0.05)
+  time.sleep(0.005)
   data: any = arduino.readline()
   return data
 
 while True:
   number: str = input("Number to send to Arduino: ")
-  value = writeReadTest(number)
+  value: str = str(writeReadTest(number))
   print(value)
